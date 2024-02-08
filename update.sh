9874b8bacc1ca6c54b870397d28db93d6199b423
Mario Illescas <mario.ille25@gmail.com>
2024-02-08 13:20:31 -0600

    adding function to update script

--- update.sh
+++ update.sh
@@ -6,13 +6,17 @@ errorlog=/var/log/updater_errors.log
 
 echo "starting"
 
+check_status() {
+   if [ $? -ne 0 ]
+   then
+       echo "and error occurred, pleaes check $errolog file"
+   fi
+}
+
 if grep -q "Arch" $release_file
 then
     sudo pacman -Syu 1>>$logifle
-    if [ $? -ne 0 ]
-    then
-        echo "an error occurred, please check $errolog file"
-    fi
+    check_status
 fi
 
 echo "checking ubuntu status"
@@ -20,15 +24,9 @@ echo "checking ubuntu status"
 if grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file
 then
     apt-get update 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "an error occurred, please check $errolog file"
-    fi
+    check_status
     apt-get dist-upgrade -y 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "an error occurred, please check $errolog file"
-    fi
+    check_status
 fi
 
 echo "ending"
